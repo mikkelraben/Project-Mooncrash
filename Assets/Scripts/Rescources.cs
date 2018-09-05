4110017b6d3bf43e8ba56a6c26b9d7902f956049
Mikkel <Mikkel@meldgaard.net>
2018-09-05 19:04:24 +0200

    Moved some shit and added script

---
+++ Assets/Scripts/Rescources.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Rescources : MonoBehaviour {
+    public Text Text;
+	// Use this for initialization
+	void Start () {
+	    //lygiuhuhpuhrui
+        //guygyug
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Text = GetComponent<Text>;
+        Text.text = "0";
+	}
+}
