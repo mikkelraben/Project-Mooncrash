3f572e3c306e0982eaa76362f768e2b0077923e1
Mikkel <Mikkel@meldgaard.net>
2018-09-19 17:54:22 +0200

    Changed som shits

--- Assets/Scripts/Rescources.cs
+++ Assets/Scripts/Rescources.cs
@@ -5,16 +5,21 @@ using UnityEngine.UI;
 
 public class Rescources : MonoBehaviour {
     public Text Text;
-
-	// Use this for initialization
-	void Start () {
-	    //lygiuhuhpuhrui
-        //guygyug
-	}
+    public float BankEfficieny, MaterialMineEfficieny;
+    public float Money, Material;
+    public int Bank, MaterialMine;
+    public float BankIncome, MaterialMineIncome;
+    public float PriceBank, PriceMaterialMine;
 	
 	// Update is called once per frame
 	void Update () {
-        //Text = GetComponent<Text>;
-        Text.text = "0";
+        PriceBank = Mathf.Pow(Bank,1.3f)+1;
+        PriceMaterialMine = Mathf.Pow(MaterialMine, 2)+1;
+        BankIncome = Bank * BankEfficieny;
+        MaterialMineIncome = MaterialMine * MaterialMineEfficieny;
+        Money = Money + BankIncome;
+        Material = Material + MaterialMineIncome;
+
 	}
+
 }
