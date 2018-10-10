3776b4b69342739ba4d96270c3b7f6df71944b71
Mikkel <Mikkel@meldgaard.net>
2018-10-10 18:56:35 +0200

    Did minor changes

--- Assets/Scripts/Mün.cs
+++ Assets/Scripts/Mün.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Mün : MonoBehaviour {
     public float MaterialOnMün;
     float MaterialOnMars;
     public RescourcesOnMars RescourcesOnMarsScript;
     public FreighterSpawn freighterSpawn;
+    public Text text;
 
 	//Use this for initialization
 	void Start () {
@@ -22,6 +24,7 @@ public class Mün : MonoBehaviour {
             RescourcesOnMarsScript.Material = RescourcesOnMarsScript.Material - 20;
             MaterialOnMün = MaterialOnMün + 20f;
             freighterSpawn.SpawnFreighter();
+            text.text = "Material On Moon: " + MaterialOnMün;
         }
     }
 }
