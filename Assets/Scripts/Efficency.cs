a0c8d3eeabf8208a522fe1a50806897a0545519e
Constructor3000 <43009397+Constructor3000@users.noreply.github.com>
2018-10-24 18:55:18 +0200

    Work on effeicency and upgrade buttons and script

--- Assets/Scripts/Efficency.cs
+++ Assets/Scripts/Efficency.cs
@@ -3,15 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Efficency : MonoBehaviour {
-    public float MineLevel, BankLevel;
-	// Use this for initialization
+    public float MineLevel, BankLevel, FreighterLevel;
+    RescourcesOnMars RescourcesOnMars;
+
 	void Start () {
-        MineLevel = 0;
-        BankLevel = 0;
+        MineLevel = 1;
+        BankLevel = 1;
+        FreighterLevel = 1;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    public void MineUpgrade()
+    {
+        if (RescourcesOnMars.Money < 100 * MineLevel)
+        {
+            RescourcesOnMars.Money = RescourcesOnMars.Money - (100 * MineLevel);
+            MineLevel++;
+        }
+    }
+
+    public void BankUpgrade()
+    {
+        if (RescourcesOnMars.Money < (100 * BankLevel))
+        {
+            RescourcesOnMars.Money = RescourcesOnMars.Money - (100 * BankLevel);
+            BankLevel++;
+        }
+    }
+
+    public void FreigtherUpgrade()
+    {
+
+    }
 }
