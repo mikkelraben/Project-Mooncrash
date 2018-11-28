a73997e59fd7c09f36a5b8ca41729a18ece28f73
Constructor3000 <43009397+Constructor3000@users.noreply.github.com>
2018-11-28 17:42:04 +0100

    Buildings

---
+++ Assets/Scripts/MarsBuildingSpawning.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarsBuildingSpawning : MonoBehaviour {
+    public Efficency Efficency;
+    public GameObject spaceColony;
+    public GameObject MarsMine;
+   
+	
+	// Update is called once per frame
+	void Update () {
+        transform.localScale.Set(Efficency.BankLevel * 0.1, Efficency.BankLevel * 0.1, Efficency.BankLevel * 0.1, );
+        }
+}
